bee3f3dd78c1b6fa6c1db5b6d88e33fbd9d7355d
gunwoongPark <olympiodoros@naver.com>
2021-03-31 16:53:22 +0900

    폰트 적용 완료

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import About from "./components/Body/About";
 import Skills from "./components/Body/Skill/SkillsContainer";
 import Project from "./components/Body/Project/ProjectContainer";
 import { createGlobalStyle } from "styled-components";
+import "./App.css";
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: NanumSquare;
-  src: local('NanumSquare_acR');
-}
 
 body{
-  font-family:'NanumSquare'
+  font-family: 'NanumSquare'
 }
 
 html{
