f9e7bb82742aceb5171faf4fee0afe9d46a4ae8b
parkGunWoong <gunwoong0807@logibros.com>
2021-08-22 19:17:50 +0900

    no message

--- src/container/SkillsContainer.js
+++ src/container/SkillsContainer.js
@@ -9,23 +9,25 @@ import cssIcon from "../assets/css.png";
 import jsIcon from "../assets/js.png";
 import vuetifyIcon from "../assets/vuetify.svg";
 import scIcon from "../assets/styled-components.png";
-import githubIcon from "../assets/github.png";
+import gitIcon from "../assets/git.png";
 import reduxIcon from "../assets/redux.png";
 import pyIcon from "../assets/python.png";
 import fbIcon from "../assets/firebase.png";
+import sassIcon from "../assets/sass.png";
 
-function SkillsContainer() {
+function SkillsContainer () {
   const skills = [
     { name: "HTML", icon: htmlIcon },
     { name: "CSS", icon: cssIcon },
     { name: "JavaScript", icon: jsIcon },
     { name: "Vue", icon: vueIcon },
     { name: "Vuetify", icon: vuetifyIcon },
+    { name: "Sass", icon: sassIcon },
     { name: "React", icon: reactIcon },
     { name: "Redux", icon: reduxIcon },
     { name: "styled components", icon: scIcon },
     { name: "Bootstrap", icon: bootstrapIcon },
-    { name: "GitHub", icon: githubIcon },
+    { name: "Git", icon: gitIcon },
     { name: "Python", icon: pyIcon },
     { name: "Firebase", icon: fbIcon },
   ];
